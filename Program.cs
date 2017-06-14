bd97cbf9a8f222be3a655bae62a6a500ceec6444
Pedro Mota <phomota@hotmail.com>
2017-06-14 13:04:06 +0100

    Changed to use the id instead of matching the url directly all the time

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace consoleWebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WebClient client = new WebClient();
+            HtmlDocument html = new HtmlDocument();
+            Console.WriteLine("enter the imgur url: ");
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Download starting");
+            string source = client.DownloadString(url);
+
+            html.LoadHtml(source);
+            //Get all divs that contains the link to the images
+            HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//div[@id='inside']/div[2]/div[2]/div[2]/div");
+            string folder = url.Substring(url.LastIndexOf("/") + 1);
+            try
+            {
+                Directory.CreateDirectory(folder);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Folder already exists, do you wish to remove it to continue with the download?\n1-yes 2-no");
+                int op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1: //Delete current folder
+                    {
+                            Directory.Delete(folder);
+                            Directory.CreateDirectory(folder);
+                            break;
+                    }
+                    default: //If the user choose anything other than 1 close the application
+                        {
+                            Console.WriteLine("Closing application");
+                            System.Threading.Thread.Sleep(5000);
+                            System.Environment.Exit(0);
+                            break;
+                        }
+                }
+
+            }
+            for (int i = 0; i < nodes.Count-1; i++)
+            {
+                Console.WriteLine("Downloading {0} of {1}",i+1,nodes.Count);
+                url = "http://i.imgur.com/" + nodes[i].Id + ".jpg"; //Get the unique img id and insert on the pre-formatted url to download the img
+                client.DownloadFile(url, folder + "\\" + i + ".jpg");
+           }
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ImgGurDownloader
+Just compile and run the code, the prompt will ask you for the imgur link. On this version, the link must be correctly formatted.
+The expected format is "http://imgur.com/?/??????"
